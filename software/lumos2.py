f0ba2d7a7be00d76ee9be2c7f88e4db6752bae74
Aurora CanSat 2016 <cansataurora@gmail.com>
2016-01-14 20:00:48 +0000

    Update lumos2.py

--- software/lumos2.py
+++ software/lumos2.py
@@ -1,3 +1,4 @@
+# main.py -- put your code here!
 #########################
 #        Imports        #
 #########################
@@ -12,8 +13,9 @@ from time import sleep
 #     Prerequisites     #
 #########################
 
+
 #create BMP180 object
-bmp180 = BMP180()
+bmp180 = BMP180('X')
 bmp180.oversample_sett = 3 #0=low accuracy, 3=high accuracy
 bmp180.baseline = 101325 #pressure at main sea level
 
@@ -25,10 +27,12 @@ x3_pin.high()
 hc12 = UART(4, 9600)
 
 #feedback-pyboard on and working
-LED(4).on
+green = LED(2)
+green.on()
+blue = LED(4)
+orange = LED(3)
 
-#open backup.csv to write data to
-backup = open('/sd/backup.csv', 'w')
+finished = False
 
 
 #########################
@@ -36,17 +40,41 @@ backup = open('/sd/backup.csv', 'w')
 #########################
 
 
-#50s loop, get data every half second and write to backup.csv, and also transmit to ground station
-for tag in range(50):
-	temp = bmp180.temperature
-	pres = bmp180.pressure
-	alt = bmp180.altitude
-	backup.write('{},{},{},{},{}\n'.format(tag,temp,pres,alt,location))
+while finished == False:
 
-	data = str(tag) + ',' + str(temp) + ',' + str(pres) + ',' + str(alt) + ',' + str(location) #concatenate data with commas
-	hc12.write(data) #write data over UART4 to transmit to ground station
+	#if start command is received
+	if hc12.any():
+		orange.off()
 
-	sleep(1) #sleep for a second to buffer
+		for i in range(0,2):
+			blue.toggle()
+			sleep(0.2)
+			blue.toggle()
+			sleep(0.2)
+
+		#50s loop, get data every half second and write to backup.csv, and also transmit to ground station
+		for tag in range(1,11):
+			
+			temp = bmp180.temperature
+			pres = bmp180.pressure
+			alt = bmp180.altitude
+			location = 0
+
+			#open backup.csv to write data to, write to it, then close it
+			backup = open('/sd/backup.csv', 'a')
+			backup.write('{},{},{},{},{}\n'.format(tag,temp,pres,alt,location))
+			backup.close()
+
+			data = str(tag) + ',' + str(temp) + ',' + str(pres) + ',' + str(alt) + ',' + str(location) #concatenate data with commas
+
+			hc12.write(data) #write data over UART4 to transmit to ground station
+
+			sleep(1) #sleep for a second to buffer
+
+		finished = True
+
+	else:
+		orange.on()
 
 
 #########################
@@ -54,5 +82,10 @@ for tag in range(50):
 #########################
 
 
-backup.close()
-LED(4).off
+for i in range(0,4):
+	green.toggle()
+	sleep(0.2)
+	green.toggle()
+	sleep(0.2)
+	
+green.off()
