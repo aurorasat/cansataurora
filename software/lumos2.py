210468c5099d39898912afa942deb7a1e5237761
Aurora CanSat 2016 <cansataurora@gmail.com>
2016-01-24 16:10:01 +0000

    Update lumos2.py

--- software/lumos2.py
+++ software/lumos2.py
@@ -5,7 +5,7 @@
 
 
 from bmp180 import BMP180
-from pyb import UART, LED, Pin, millis
+from pyb import UART, LED, Pin
 from time import sleep
 from micropyGPS import MicropyGPS
 
@@ -42,11 +42,10 @@ blue = LED(4)
 
 #feedback-waiting for user to press button
 orange = LED(3)
-orange.off()
+orange.on()
 
 #boolean variable to manage main loop
 finished = False
-
 
 #########################
 #       Main Loop       #
@@ -58,42 +57,49 @@ while finished == False:
 	if hc12.any():
 		orange.off()
 
+		#for local use only
 		for i in range(0,2):
 			blue.toggle()
-			sleep(0.2)
+			sleep(0.4)
 			blue.toggle()
-			sleep(0.2)
+			sleep(0.4)
 
-		#50s loop, get data every half second and write to backup.csv, and also transmit to ground station
-		for tag in range(1,11):
+		#X second loop, get data every half second and write to backup.csv, and also transmit to ground station
+		for tag in range(1,61):
+
+			green.off()
 			
 			temp = bmp180.temperature
 			pres = bmp180.pressure
-			
+			alt = bmp180.altitude
 
 			#if there is gps data to be read
-			if uart.any():
-				my_sentence = uart.readline()
+			while uart.any():
+				my_sentence = chr(uart.readchar())
 
 			for x in my_sentence:
 				my_gps.update(x)
 
 			latitude = my_gps.latitude_string()
 			longitude = my_gps.longitude_string()
-			alt = my_gps.altitude
+			timestamp = my_gps.timestamp
+			#alt2 = my_gps.altitude
 
 			#open backup.csv to write data to, write to it, then close it
 			backup = open('/sd/backup.csv', 'a')
-			backup.write('{},{},{},{},{},{}\n'.format(tag,temp,pres,alt,latitude,longitude))
+			backup.write('{},{},{},{},{},{},{}\n'.format(tag,timestamp,temp,pres,alt,latitude,longitude))
 			backup.close()
 
 			data = str(tag) + ',' + str(temp) + ',' + str(pres) + ',' + str(alt) + ',' + str(latitude) + ',' + str(longitude) #concatenate data with commas
 
 			hc12.write(data) #write data over UART4 to transmit to ground station
 
+			green.on()
 			sleep(1) #sleep for a second to buffer
 
 		finished = True
+		sleep(2)
+		hc12.write('end')
 
 
 #########################
@@ -102,9 +108,10 @@ while finished == False:
 
 
 for i in range(0,4):
-	green.toggle()
+	blue.toggle()
 	sleep(0.2)
-	green.toggle()
+	blue.toggle()
 	sleep(0.2)
 	
 green.off()
+blue.off()
